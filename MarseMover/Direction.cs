35b12f913765ab8c54291d6ab36c49fa78600c5c
vinothkanth <vinothkanth@5gindia.net>
2018-09-11 16:56:18 +0530

    MarseMover

---
+++ MarseMover/Direction.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarceRover
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    enum Degree
+    {
+        N = 0,
+        E = 90,
+        S = 180,
+        W = 270
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    enum CommanDirection
+    {
+        North = 'N',
+        East = 'E',
+        West = 'W',
+        South = 'S'
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    enum RelativeDirection
+    {
+        Left = 'L',
+        Right = 'R'
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Direction
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="degree"></param>
+        /// <returns></returns>
+        public static char ToName(int degree)
+        {
+            char key = ' ';
+            try
+            {
+                switch (degree)
+                {
+                    case (int)Degree.N:
+                        key = Degree.N.ToString().ToCharArray()[0];
+                        break;
+
+                    case (int)Degree.E:
+                        key = Degree.E.ToString().ToCharArray()[0];
+                        break;
+
+                    case (int)Degree.S:
+                        key = Degree.S.ToString().ToCharArray()[0];
+                        break;
+
+                    case (int)Degree.W:
+                        key = Degree.W.ToString().ToCharArray()[0];
+                        break;
+
+                    default:
+                        throw new Exception("Degree Contains only 0,90,180 and 270");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw new Exception(ex.Message);
+            }
+
+            return key;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static int ToDegree(char key)
+        {
+            int degree = 0;
+            try
+            {
+                switch (char.ToUpper(key))
+                {
+                    case (char)CommanDirection.North:
+                        degree = (int)Degree.N;
+                        break;
+
+                    case (char)CommanDirection.East:
+                        degree = (int)Degree.E;
+                        break;
+
+                    case (char)CommanDirection.South:
+                        degree = (int)Degree.S;
+                        break;
+
+                    case (char)CommanDirection.West:
+                        degree = (int)Degree.W;
+                        break;
+
+                    default:
+                        throw new Exception("Direction Contains only E,W,N and S");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw new Exception(ex.Message);
+            }
+
+            return degree;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public static char GetNextDirectionFromLeft(char direction)
+        {
+            char key= char.ToUpper(direction);
+            char nextDegree = ' ';
+            try
+            {
+                if (key == (char)CommanDirection.East || key == (char)CommanDirection.West || key == (char)CommanDirection.South)
+                {
+                    nextDegree = ToName(ToDegree(direction) - 90);
+                }
+                else if (key == (char)CommanDirection.North)
+                {
+                    nextDegree = 'W';
+                }
+                else
+                {
+                    throw new Exception("Direction Contains only E,W,N and S");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw new Exception(ex.Message);
+            }
+
+            return nextDegree;
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public static char GetNextDirectionFromRight(char direction)
+        {
+            char key = char.ToUpper(direction);
+            char nextDegree = ' ';
+            try
+            {
+                if (key == (char)CommanDirection.East || key == (char)CommanDirection.North || key == (char)CommanDirection.South)
+                {
+                    nextDegree = ToName(ToDegree(direction) + 90);
+                }
+                else if (key == (char)CommanDirection.West)
+                {
+                    nextDegree = 'N';
+                }
+                else
+                {
+                    throw new Exception("Direction Contains only E,W,N and S");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw new Exception(ex.Message);
+            }
+
+            return nextDegree;
+
+        }        
+    }
+}
---
+++ MarseMover/Rover.cs
@@ -0,0 +1,146 @@
+﻿// Problem Title : Marse Rover
+// Starting Date :
+//
+//
+//
+//
+
+namespace MarceRover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// The Rover Class
+    /// </summary>
+    public class Rover
+    {
+        public int Id { get; set; }
+
+        public int XPoint { get; set; }
+
+        public int YPoint { get; set; }
+
+        public char DirectionKey { get; set; }
+
+
+        /// <summary>
+        /// To set rover position
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="xPoint">X Point</param>
+        /// <param name="yPoint">Y Point</param>
+        /// <param name="direction">Direction</param>
+        public void SetPosition(int id, int xPoint, int yPoint, char direction)
+        {
+            Id = id;
+            XPoint = xPoint;
+            YPoint = yPoint;
+            DirectionKey = char.ToUpper(direction);
+        }
+
+        /// <summary>
+        /// To get the current position of th rover
+        /// </summary>
+        /// <returns></returns>
+        public string GetCurrentPosition()
+        {
+            return string.Format("{0} {1} {2}", XPoint, YPoint, DirectionKey);
+        }
+
+
+        public void ResetXPoint(int point)
+        {
+            XPoint = point;
+        }
+
+        public void ResetYPoint(int point)
+        {
+            YPoint = point;
+        }
+
+        public void ResetDirection(char direction)
+        {
+            DirectionKey = direction;
+        }
+
+        /// <summary>
+        /// To get next direction of given command
+        /// </summary>
+        /// <param name="currentDirection">direction</param>
+        /// <param name="command">command(L/R)</param>
+        /// <returns>next direction</returns>
+        public char GetNextDirection(char currentDirection, char command)
+        {
+            char nextDirection = ' ';
+            try
+            {
+                if (char.ToUpper(command) == (char)RelativeDirection.Left)
+                {
+                    nextDirection = Direction.GetNextDirectionFromLeft(currentDirection);
+                }
+                else if (char.ToUpper(command) == (char)RelativeDirection.Right)
+                {
+                    nextDirection = Direction.GetNextDirectionFromRight(currentDirection);
+                }
+                else
+                {
+                    throw new ArgumentException("The command only contain L and R");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+
+            return nextDirection;
+        }
+
+        /// <summary>
+        /// To get next point from givent points
+        /// </summary>
+        /// <param name="xPoint">X Point</param>
+        /// <param name="yPoint">Y Point</param>
+        /// <param name="currentDirection">Direction</param>
+        /// <param name="step">steps</param>
+        /// <returns>next point</returns>
+        public int[] GetNextPoint(int xPoint, int yPoint, char currentDirection, int step)
+        {
+            int[] nextPoint = new int[2] { xPoint , yPoint };
+            try
+            {
+                switch (char.ToUpper(currentDirection))
+                {
+                    case (char)CommanDirection.East:
+                        nextPoint[0] = Point.Increament(xPoint, step);
+                        break;
+
+                    case (char)CommanDirection.West:
+                        nextPoint[0] = Point.Decrement(xPoint, step);
+                        break;
+
+                    case (char)CommanDirection.North:
+                        nextPoint[1] = Point.Increament(yPoint, step);
+                        break;
+
+                    case (char)CommanDirection.South:
+                        nextPoint[1] = Point.Decrement(yPoint, step);
+                        break;
+
+                    default:
+
+                        throw new ArgumentException("The direction key not in exsist");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+
+            return nextPoint;
+        }
+    }
+}
